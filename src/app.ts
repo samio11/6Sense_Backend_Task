8cc99e56a805a675b67dda40ace3757153bc762b
samio11 <samiohasan6@gmail.com>
2026-04-17 22:04:55 +0600

    Custom Error handler added and also for image upload multer and cloudinary config  added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import { notFound } from './app/middlewares/notFound';
 const app:Application = express();
 
 app.use(express.json());
@@ -13,4 +15,6 @@ app.get("/",(req:Request,res:Response)=>{
     });
 })
 
+app.use(globalErrorHandler)
+app.use(notFound)
 export default app;
